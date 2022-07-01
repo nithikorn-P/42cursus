18022eb3ddeec88c30ce42d4a78c22b40b221f2a
Nithikorn Dejsongchan <ndejsong@z3t1c4.42bangkok.com>
2022-07-01 13:06:05 +0700

    feat: bla bla bla

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndejsong <ndejsong@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 10:22:04 by ndejsong          #+#    #+#             */
+/*   Updated: 2022/07/01 12:49:47 by ndejsong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	srcsize;
+
+	i = 0;
+	j = 0;
+	srcsize = ft_strlen(src);
+	if (dstsize == 0)
+		return (srcsize);
+	while (dst[i])
+		i++;
+	if (i >= dstsize)
+		return (dstsize + srcsize);
+	while (src[j] && j < dstsize - i - 1)
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	dst[i + j] = 0;
+	return (i + srcsize);
+}
