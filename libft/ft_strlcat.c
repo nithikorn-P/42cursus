be48535026074ea5ba16cf77952992e59757c6ea
nithikorn-P <nithikorn.dej@gmail.com>
2022-08-05 13:09:55 +0700

    feat: some bugs

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -16,20 +16,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
-	size_t	srcsize;
 
-	i = 0;
-	j = 0;
-	srcsize = ft_strlen(src);
-	while (dst[i])
-		i++;
+	i = ft_strlen(dst);
 	if (i >= dstsize)
-		return (dstsize + srcsize);
-	while (src[j] && j < dstsize - i - 1)
+		return (dstsize + ft_strlen(src));
+	j = 0;
+	while (j < dstsize - i - 1 && src[j])
 	{
 		dst[i + j] = src[j];
 		j++;
 	}
 	dst[i + j] = 0;
-	return (i + srcsize);
+	return (i + ft_strlen(src));
 }
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -14,13 +14,15 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t		i;
+	unsigned char	diff;
 
 	i = 0;
-	while (i < n)
+	while (i < n && (s1[i] || s2[i]))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		diff = (unsigned char) s1[i] - (unsigned char) s2[i];
+		if (diff != 0)
+			return (diff);
 		i++;
 	}
 	return (0);
