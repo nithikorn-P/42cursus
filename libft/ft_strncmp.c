9e2a0fbb0421eda1e66f733fee6311130d634be3
nithikorn-P <nithikorn.dej@gmail.com>
2022-08-08 22:33:57 +0700

    final bug

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -14,16 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t		i;
-	unsigned char	diff;
+	size_t	i;
 
 	i = 0;
-	while (i < n && (s1[i] || s2[i]))
+	if (n == 0)
+		return (0);
+	while (s1[i] && i < n - 1)
 	{
-		diff = (unsigned char) s1[i] - (unsigned char) s2[i];
-		if (diff != 0)
-			return (diff);
+		if ((unsigned char) s1[i] - (unsigned char) s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
