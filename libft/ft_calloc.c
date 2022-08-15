ec3baefa245088b60245da66ef72b9e6a6aa4215
nithikorn-P <nithikorn.dej@gmail.com>
2022-08-15 19:27:12 +0700

    fix: handler

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ndejsong <ndejsong@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 18:04:15 by ndejsong          #+#    #+#             */
-/*   Updated: 2022/07/05 12:15:54 by ndejsong         ###   ########.fr       */
+/*   Updated: 2022/08/15 18:19:11 by ndejsong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	memsize;
 
 	memsize = count * size;
-	if (memsize < count || memsize < size)
-		return (NULL);
+	if (memsize < count && memsize < size)
+		return (0);
 	if (count == 0 || size == 0)
 		memsize = 1;
 	p = malloc(memsize);
-	if (p)
-		ft_bzero(p, memsize);
+	if (!p)
+    return (0);
+	ft_bzero(p, memsize);
 	return (p);
 }
